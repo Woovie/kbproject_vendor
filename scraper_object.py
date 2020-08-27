bcf2a362ef3e8c5318c821e16e704e2fb80f1ebb
Jordan Banasik <woovie@woovie.net>
2020-08-26 20:10:25 -0700

    Remove old base class

--- scraper_object.py
+++
@@ -1,8 +0,0 @@
-import uuid
-
-class ScraperObject():
-    def __init__(self):
-        pass
-
-    def set_id(self, namespace_uuid: uuid.UUID, name: str):
-        self.uuid = uuid.uuid5(namespace_uuid, name)
