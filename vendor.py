8d0075e7a7dfe31e3d50cb6e4b9490e113e6d8c7
Jordan Banasik <woovie@woovie.net>
2020-08-26 20:40:15 -0700

    Add intaking vendor dict to Vendor class
    This is necessary to init a vendor so we can use the data for other
    various purposes.

--- vendor.py
+++ vendor.py
@@ -3,8 +3,9 @@ import re, configparser
 from kbproject_vendor import baseclass, product
 
 class Vendor(baseclass.BaseClass):
-    def __init__(self):
+    def __init__(self, vendor_dict: dict):
         self.price_regex = re.compile(r"(\d{1,3}\,){0,3}\d{1,3}(\.\d{2})?")
+        self.vendor_dict = vendor_dict
 
 class ShopifyVendor(Vendor):
     def set_data(self, config: configparser.ConfigParser, url: str):
