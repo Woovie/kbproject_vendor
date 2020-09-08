2ac0be2af577c00693d48f772762c64f5d545e8c
Jordan Banasik <woovie@woovie.net>
2020-09-07 21:24:45 -0700

    Significant Vendor object changes
    Added products list as default property.
    
    Added name, url, cms, scrape as individual properties.
    
    Added json properties.
    
    Removed URL as part of setting data in Shopify.
    
    Added UUID setting from baseclass.BaseClass.
    
    Added methods to process vendor creation.

--- vendor.py
+++ vendor.py
@@ -1,14 +1,36 @@
-import re, configparser
+import re, configparser, json, uuid, hashlib
 
 from kbproject_vendor import baseclass, product
 
 class Vendor(baseclass.BaseClass):
     def __init__(self, vendor_dict: dict):
         self.price_regex = re.compile(r"(\d{1,3}\,){0,3}\d{1,3}(\.\d{2})?")
-        self.vendor_dict = vendor_dict
+        self.name = vendor_dict["name"]
+        self.url = vendor_dict["url"]
+        self.cms = vendor_dict["cms"]
+        self.scrape = vendor_dict["scrape?"]
+        self.products = []
+        self.json = []
+
+    def process_scraped_data(self):
+        pass
 
 class ShopifyVendor(Vendor):
-    def set_data(self, config: configparser.ConfigParser, url: str):
+    def set_data(self, config: configparser.ConfigParser):
         self.config = config
-        self.url = url
         self.scrape_url = f"https://{self.url}{self.config['shopify']['data_location']}"
+        self.uuid = uuid.uuid5(uuid.NAMESPACE_URL, self.url)
+
+    def process_scraped_data(self, body: str):
+        self.json = json.loads(body)['products']
+
+    def build_products(self):
+        for product in self.json:
+            self.products.append(self.build_product(product))
+    
+    def build_product(self, product_dict: dict):
+        product_object = product.Product().create_product("shopify")
+        product_object.set_data(self.config, product_dict)
+        product_object.set_id(self.uuid, product_object.json["title"])# ID must come first as it's needed for builds that are triggered by the product itself.
+        product_object.build()
+        return product_object
